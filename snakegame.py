9bd3d9d501782e3b84f9a5c041461f68f5278a58
dvddoge <davidjmacedo@hotmail.com>
2024-08-10 11:38:26 -0300

    delete gameLoop and create play_step

--- snakegame.py
+++ snakegame.py
@@ -81,77 +81,20 @@ class SnakeGameAI:
         else:
             return False
 
+    def play_step(self, action):
+        self._move(action)
+        self.snake.append(self.head)
+        if len(self.snake) > self.length_of_snake:
+            del self.snake[0]
 
-def gameLoop(frame_iteration):
-    game_over = False
-    frame_iteration += 1
-
-    x1 = dis_width / 2
-    y1 = dis_height / 2
-
-    x1_change = 0       
-    y1_change = 0
-
-    snake_list = []
-    length_of_snake = 1
-
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-
-    while not game_over:
-
-        text = font.render("Score ", True, white)
-        dis.blit(text, [dis_width / 2, dis_height / 2])
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
-
+        if self._is_collision():
+            return -10, True, self.score
+        
         reward = 0
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            frame_iteration = 0
-            game_over = True
-            
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(white)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-        snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
-        snake_list.append(snake_head)
-        if len(snake_list) > length_of_snake:
-            del snake_list[0]
-
-        for x in snake_list[:-1]:
-            if x == snake_head or frame_iteration > 100 * len(snake_list):
-                frame_iteration = 0
-                reward = -10
-                game_over = True
-
-        our_snake(snake_block, snake_list)
-        pygame.display.update()
-
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+        if(self.head) == self.food:
+            self.length_of_snake += 1
+            self.score += 1
             reward = 10
-            length_of_snake += 1
+            self._place_food()
 
-        clock.tick(snake_speed)
-
-gameLoop(frame_iteration)
+        return reward, False, self.score
