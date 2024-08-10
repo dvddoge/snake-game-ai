e0ec676dbf5af8dba791d02fb53e5057eee23a84
dvddoge <davidjmacedo@hotmail.com>
2024-08-10 13:32:45 -0300

    QTrainer init

--- model.py
+++ model.py
@@ -23,3 +23,10 @@ class Lienar_QNet(nn.Module):
         file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
 
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimazer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
