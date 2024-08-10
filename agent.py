6034defcf5029a68c6fad9117702e42dfa953afc
dvddoge <davidjmacedo@hotmail.com>
2024-08-10 12:00:35 -0300

    Initial base functions

--- agent.py
+++ agent.py
@@ -7,3 +7,24 @@ from snakegame import SnakeGameAI, Direction, Point
 MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+
+class Agent:
+    def _init_(self):
+        self.game = SnakeGameAI()
+
+    def get_state(self, game):
+        pass
+
+    def select_action(self, state):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
